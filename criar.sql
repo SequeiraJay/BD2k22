a22a5ba1f2af003779b8608ea9398b4927d78348
João Sequeira <93724639+SequeiraJay@users.noreply.github.com>
2022-11-17 17:08:31 +0000

    Update criar.sql

--- criar.sql
+++ criar.sql
@@ -14,10 +14,11 @@ DROP TABLE IF EXISTS Golo;
 --CREATE TABLES --
 
 CREATE TABLE Jogador(
-        idJogador INTEGER PRIMARY KEY,
-        nome      TEXT NOT NULL,
-        dataNasc  DATE,
-        idEquipa  INTEGER REFERENCES Equipa(idEquipa)
+        idJogador       INTEGER PRIMARY KEY,
+        nome            TEXT NOT NULL,
+        nacionalidade   TEXT NOT NULL,
+        dataNasc        DATE,
+        idEquipa        INTEGER REFERENCES Equipa(idEquipa)
     );
 
 CREATE TABLE Equipa(
@@ -38,7 +39,8 @@ CREATE TABLE Clube(
 
 CREATE TABLE Jogo (
     idJogo      INTEGER PRIMARY KEY,
-    jornada     TEXT NOT NULL,
+    jornada     INTEGER NOT NULL,
+    resultado   TEXT NOT NULL,
     data_jogo   DATE NOT NULL,
     idEquipa    INTEGER REFERENCES Equipa(idEquipa),
     idPavilhão  INTEGER REFERENCES Pavilhão(idPavilhão)
@@ -48,7 +50,7 @@ CREATE TABLE Jogo (
     
 CREATE TABLE Golo(
     idGolo      INTEGER PRIMARY KEY,
-    tipodeGolo  TEXT,
+
     idJogador   INTEGER REFERENCES Jogador(idJogador),
     idEquipa    INTEGER REFERENCES Equipa (idEquipa),
     idJogo      INTEGER REFERENCES Jogo(idJogo)
@@ -60,17 +62,3 @@ CREATE TABLE Pavilhão(
 
 
 );
-
-
-
-
-Jogador( idJogador, nome, dataNasc, nacionalidade, idEquipa -> Equipa
-Equipa( idEquipa, nome, idClube -> Clube       //////////
-Clube( idClube, nome, morada, dataFundação, idPavilhão -> Pavilhão   ////////
-Jogo( idJogo , jornada, data_jogo, idEquipa -> Equipa, idPavilhão -> Pavilhão /////////
-Pavilhão( idPavilhão, nome          /////
-Golo( idGolo, idJogador ->Jogador, idEquipa ->Equipa, idJogo -> Jogo //////
-
-
-
-ATENÇAO TIPO DE GOLO~~~~~~
