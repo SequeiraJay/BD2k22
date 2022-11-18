4dd56bba5b0ac121ea5e45710c307754abd18c42
João Sequeira <93724639+SequeiraJay@users.noreply.github.com>
2022-11-18 10:50:01 +0000

    Update criar.sql

--- criar.sql
+++ criar.sql
@@ -18,14 +18,14 @@ CREATE TABLE Jogador(
         nome            TEXT CONSTRAINT nn_nome_jogador NOT NULL,
         nacionalidade   TEXT CONSTRAINT nn_nacionalidade_jogador NOT NULL,
         dataNasc        DATE CONSTRAINT nn_dataNascimento_jogador NOT NULL,
-        idEquipa        INTEGER REFERENCES Equipa(idEquipa)
+        idEquipa        INTEGER CONSTRAINT fk_equipa REFERENCES Equipa(idEquipa)
     );
 
 CREATE TABLE Equipa(
-    idEquipa  INTEGER PRIMARY KEY,
-    nome      TEXT CONSTRAINT nn_nome_equipa NOT NULL CONSTRAINT UNIQUE,
+    idEquipa       INTEGER PRIMARY KEY,
+    nome           TEXT CONSTRAINT nn_nome_equipa NOT NULL,
     classificação  INTEGER CONSTRAINT check_max_class CHECK((classificação > 0 AND classificação <=16)) CONSTRAINT nn_class_equipa NOT NULL,
-    idClube   INTEGER REFERENCES Clube(idClube)
+    idClube        INTEGER CONSTRAINT fk_clube REFERENCES Clube(idClube)
 
 
 );
@@ -35,25 +35,27 @@ CREATE TABLE Clube(
     nome         TEXT CONSTRAINT nn_nome_clube NOT NULL,
     morada       TEXT,
     dataFundação TEXT,
-    idPavilhão   INTEGER REFERENCES Pavilhão(idPavilhão)
+    idPavilhão   INTEGER CONSTRAINT fk_pavilhão REFERENCES Pavilhão(idPavilhão)
 );
 
 CREATE TABLE Jogo (
     idJogo      INTEGER PRIMARY KEY,
+    visitante   TEXT CONSTRAINT id_equipa_visitante REFERENCES Equipa(idEquipa),
+    visitado    TEXT CONSTRAINT id_equipa_visitada  REFERENCES Equipa(idEquipa),
     jornada     INTEGER CONSTRAINT  nn_jornada CHECK((jornada >0 AND jornada <= 30)),
     resultado   TEXT CONSTRAINT nn_resultado_jogo NOT NULL,
-    data_jogo   DATE CONSTRAINT nn_datajogo_jogo NOT NULL,
-    idEquipa    INTEGER REFERENCES Equipa(idEquipa),
-    idPavilhão  INTEGER REFERENCES Pavilhão(idPavilhão)
+    data        DATE CONSTRAINT nn_datajogo_jogo NOT NULL,
+    idEquipa    INTEGER CONSTRAINT fk_equipa REFERENCES Equipa(idEquipa),
+    idPavilhão  INTEGER CONSTRAINT fk_pavilhão REFERENCES Pavilhão(idPavilhão)
 
 
 );
     
 CREATE TABLE Golo(
     idGolo      INTEGER PRIMARY KEY,
-    idJogador   INTEGER REFERENCES Jogador(idJogador),
-    idEquipa    INTEGER REFERENCES Equipa (idEquipa),
-    idJogo      INTEGER REFERENCES Jogo(idJogo)
+    idJogador   INTEGER CONSTRAINT fk_jogador REFERENCES Jogador(idJogador),
+    idEquipa    INTEGER CONSTRAINT fk_equipa REFERENCES Equipa (idEquipa),
+    idJogo      INTEGER CONSTRAINT fk_jogo REFERENCES Jogo(idJogo)
 );
     
 CREATE TABLE Pavilhão(
@@ -61,6 +63,3 @@ CREATE TABLE Pavilhão(
     nome        TEXT CONSTRAINT nn_nome_pavilhão NOT NULL
 
 );
-
-
-
